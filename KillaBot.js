86fd1d9123eb250549e8b0816162ff5857c5a8b8
tmati <38438014+tmati@users.noreply.github.com>
2021-01-17 13:33:02 +0200

    updated urls after Woods expansion
    added reserve underground map to !reserve responses.

--- KillaBot.js
+++ KillaBot.js
@@ -15,35 +15,35 @@ bot.on('message', msg => {
         case '!interchange':
         case '!int':
         case '!Interchange':
-            msg.channel.send('https://gamepedia.cursecdn.com/escapefromtarkov_gamepedia/5/5d/InterchangeMapLorathorDark.jpg');
+            msg.channel.send('https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/e/e5/InterchangeMap_Updated_4.24.2020.png/revision/latest?cb=20200424115934');
             break;
         case '!customs':
         case '!cus':
         case '!Customs':
-            msg.channel.send('https://escapefromtarkov.gamepedia.com/File:Customs_Nuxx_20190106_1.2.png');
+            msg.channel.send('https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/5/55/CustomsLargeExpansionGloryMonki.png/revision/latest?cb=20200805222908');
             break;
         case '!labs':
         case '!Labs':
-            msg.channel.send('https://escapefromtarkov.gamepedia.com/File:LabsMapNehalemX.webp');
+            msg.channel.send('https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/d/d7/The_Lab_Map_%28EN%29.png/revision/latest?cb=20200329133328');
             break;
         case '!Shoreline':
         case '!shoreline':
         case '!sho':
         case '!sl':
-            msg.channel.send('https://gamepedia.cursecdn.com/escapefromtarkov_gamepedia/d/d4/Shoreline_marvelin_2_updated.png?version=f28651df0d566bdc1996aeeacb496d15');
+            msg.channel.send('https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/d/d4/Shoreline_marvelin_2_updated.png/revision/latest?cb=20191120000235');
             break;
         case '!factory':
         case '!Factory':
-            msg.channel.send('https://gamepedia.cursecdn.com/escapefromtarkov_gamepedia/5/5c/Escape_from_Tarkov_Factory_Map_by_stealtheh.jpg?version=2710f1b141acdd8916616205fe6b7370');
+            msg.channel.send('https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/5/5c/Escape_from_Tarkov_Factory_Map_by_stealtheh.jpg/revision/latest?cb=20191005203908');
             break;
         case '!Woods':
         case '!woods':
-            msg.channel.send('https://gamepedia.cursecdn.com/escapefromtarkov_gamepedia/0/06/WoodsMapKeySpawnsExits.jpg?version=ddc2b73e366c043da5f01ebd3a1fd532');
+            msg.channel.send('https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/0/05/Glory4lyfeWoods_map_v4_marked.png/revision/latest?cb=20210103091918');
             break;
         case '!Reserve':
         case '!reserve':
         case '!res':
-            msg.channel.send('https://gamepedia.cursecdn.com/escapefromtarkov_gamepedia/c/c9/Loot_.jpg?version=5e9a6784c341bca89d918b385bc331ae')
+            msg.channel.send('https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/4/42/3D_Map_by_loweffortsaltbox.png/revision/latest?cb=20200410160036, Underground map: https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/d/d0/ReserveExpandedUnderground.png/revision/latest?cb=20201004224828')
             break;
         case '!ammo':
             msg.channel.send('https://escapefromtarkov.gamepedia.com/Ballistics');
